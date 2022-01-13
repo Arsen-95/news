866c33b504a9c903609b9a562a08929a2791ecba
Arsen-95 <83858217+Arsen-95@users.noreply.github.com>
2022-01-13 21:11:49 +0500

    Update AppRouter.js

--- src/Components/AppRouter.js
+++ src/Components/AppRouter.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import BestStories from './BestStories';
 import NewStories from './NewStories';
+import PostAbout from './PostAbout';
 import TopStories from './TopStories';
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path='/TopStories' element={<TopStories/>}/>
+            <Route path='/TopStories' element={<TopStories/>} />
+            <Route path='/TopStories/:id' element={<PostAbout/>} />
             <Route path='/NewStories' element={<NewStories/>} />
+            <Route path='/NewStories/:id' element={<PostAbout/>} />
             <Route path='/BestStories' element={<BestStories/>} />
+            <Route path='/BestStories/:id' element={<PostAbout/>} />
         </Routes>
     )
 };
