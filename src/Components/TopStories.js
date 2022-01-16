acbf04108c17c8c8a07b234744d748b5f4de5485
Arsen-95 <83858217+Arsen-95@users.noreply.github.com>
2022-01-16 12:16:47 +0500

    Update TopStories.js

--- src/Components/TopStories.js
+++ src/Components/TopStories.js
@@ -30,7 +30,7 @@ const TopStories = () => {
         // }
     };
         
-    useEffect( async () => {
+    useMemo( async () => {
         setLoad(true);
         let ids = await getIds();      
         await getPosts(ids);
